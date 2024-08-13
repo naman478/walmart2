670e56289367c9e525ea1f0230247624b7f1aa5b
naman478 <namanjhanwar953@gmail.com>
2024-08-13 12:30:31 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "../src/components/Home"
+import {Routes,Route} from "react-router-dom"
+import Gift from './components/Gift/Gift';
+import Wheel from './components/Wheel/Wheel';
+import GiftCard from './components/GiftCard/GiftCard';
+import Female from './components/Female/Female';
+import Shipping from './components/Shipping/Shipping';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/gift" element={<Gift/>}/>
+        <Route path="/wheel" element={<Wheel/>}/>
+        <Route path="/giftcard" element={<GiftCard/>}/>
+        <Route path="/giftcard/female" element={<Female/>}/>
+        <Route path="/shipping" element={<Shipping/>}/>
+        {/* <Route path="/signup" element={<Signup/>} />
+        <Route path="*" element={<h1>Not found</h1>} /> */}
+      </Routes>
     </div>
   );
 }
